9a327c291a7a4f9a9237f1a9a65bc246c036192c
Shunsuke Takagi (MacBook Pro) <takagishunsuke1129@gmail.com>
2021-06-17 17:56:36 +0900

    Initial Commit

---
+++ SwiftUI_tutorial/SwiftUI_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_tutorialApp.swift
+//  SwiftUI_tutorial
+//
+//  Created by Shunsuke Takagi on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
